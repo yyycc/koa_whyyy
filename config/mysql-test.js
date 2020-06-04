1ca3acf9d443c16fbddfc0193882fb835e23da74
ever <892938772@qq.com>
2020-06-04 11:58:38 +0800

    mysql2

---
+++ config/mysql-test.js
@@ -0,0 +1,11 @@
+let mysqlTest = {
+    dialect: 'mysql',
+    database: 'whyyy',
+    username: 'ever',
+    password: '050511',
+    host: '127.0.0.1',
+    port: '3306',
+
+};
+
+module.exports = mysqlTest;
